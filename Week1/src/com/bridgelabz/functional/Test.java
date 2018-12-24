8e6bca2d23a6a5b72b6dddce39673186b1621352
Admin <sushantpatwari1234@gmail.com>
2018-12-24 17:50:53 +0530

    first commit

--- Week1/src/com/bridgelabz/functional/Test.java
+++ Week1/src/com/bridgelabz/functional/Test.java
@@ -2,14 +2,21 @@ package com.bridgelabz.functional;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
+import org.junit.runner.JUnitCore;
+
+
 public class Test {
 
 	@org.junit.Test
 	public void test() {
 		EvenOdd t=new EvenOdd();
 		//int result=t.checkEven(2);
-		assertEquals(0,t.checkEven(0));
+		Assert.assertEquals(0,t.checkEven(0));
+	
 		assertTrue((t.checkEven(4)==1));
-	}
+		
 
+	
+	}
 }
--- week2/src/com/utility/week2/Utility.java
+++ week2/src/com/utility/week2/Utility.java
@@ -492,6 +492,25 @@ public static Scanner sc= new Scanner(System.in);
 					
 				}
 			}
+		 
+		 /**********calendar*****/
+		 public static int dayOfWeek1(int y,int m,int d)
+			{
+			
+			
+			int y0 = y - (14 - m) / 12;
+	        
+	        int x = y0 + y0/4 - y0/100 + y0/400;
+	        
+	        int m0 = m + 12 * ((14 - m) / 12) - 2;
+	        
+	        int d0 = (d + x + (31*m0)/12) % 7;
+			
+			return d0;	
+			}
+	
 
+		
 }
+
 
