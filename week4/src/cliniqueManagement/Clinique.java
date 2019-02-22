0f1b026063237417088df0e0e791f6631d2b0ec1
Admin <sushantpatwari1234@gmail.com>
2019-02-22 19:26:20 +0530

    oops changes done

--- week4/src/cliniqueManagement/Clinique.java
+++ week4/src/cliniqueManagement/Clinique.java
@@ -1,40 +1,29 @@
 package cliniqueManagement;
-
 import java.text.ParseException;
 import java.util.Date;
 import com.utility.Utility;
 /**
  * @author Sushant Patwari
- * @since  10/02/2019
+ * @since 10/02/2019
  * @aim runner class for clinique management
  */
-
 public class Clinique {
-static Date date;
-	
-	public static void main(String[] args) throws ParseException 
-	{ 
-		int choice=0;
-		
+	static Date date;
+	public static void main(String[] args) throws ParseException {
+		int choice = 0;
 		do {
-			System.out.println("\n1. Add Doctor or Patient or fix an Appointment");
-			System.out.println("2. Search doctor");
-			System.out.println("3. Display details");
-			System.out.println("4. Close");
-			choice=Utility.getInt();
-			switch(choice)
-			{
+			System.out.println(
+					"\n1. Add Doctor or Patient or fix an Appointment\n2. Search doctor\n3. Display details\n4. Close");
+			choice = Utility.getInt();
+			switch (choice) {
 			case 1:
-				//to add doctor or patients details or to fix appointment
-				new DoctorPatientImplementation().operation();
+				new DoctorPatientImplementation().operation(); // to add doctor or patients details or to fix appointment
 				break;
 			case 2:
-				//to search doctor details
-				new SearchingImplementation().operation();
+				new SearchingImplementation().operation(); // to search doctor details
 				break;
 			case 3:
-				//to display information
-				new InformationDisplayImplementation().operation();
+				new InformationDisplayImplementation().operation(); 	// to display information
 				break;
 			case 4:
 				System.out.println("Exiting");
@@ -42,9 +31,6 @@ static Date date;
 			default:
 				System.out.println("Invalid Entry");
 			}
-		}while(choice!=4);
-
+		} while (choice != 4);
 	}
-
 }
-
